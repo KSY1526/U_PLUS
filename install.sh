361dcd852c81e19e3279570612a9e4e39cf6971b
KSY <zan05134@naver.com>
2022-12-06 15:53:23 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip install pandas
+pip install scikit-learn
+conda install jupyter notebook
+conda install -y pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
+conda install pyg -c pyg
